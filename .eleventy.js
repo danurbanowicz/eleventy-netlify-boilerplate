d2fd01e7835482d86e693f1dcdc739246533da6a
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2019-11-09 13:46:42 +0100

    Added favicon.ico to site root to prevent Chrome/Webkit 404 error

--- .eleventy.js
+++ .eleventy.js
@@ -52,6 +52,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
+  eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
