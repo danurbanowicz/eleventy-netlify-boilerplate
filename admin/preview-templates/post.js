948108d39579a45be17052e983117f50e373a961
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2019-11-09 11:06:50 +0100

    Updated dependencies

--- admin/preview-templates/post.js
+++ admin/preview-templates/post.js
@@ -1,5 +1,5 @@
 import htm from "https://unpkg.com/htm?module";
-import format from "https://unpkg.com/date-fns@2.0.0-alpha.2/esm/format/index.js?module";
+import format from "https://unpkg.com/date-fns@2.7.0/esm/format/index.js?module";
 
 const html = htm.bind(h);
 
