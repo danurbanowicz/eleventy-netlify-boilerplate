c9618b59b8a40304f7baa054c3c18b1b39267ad7
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2020-10-15 14:11:46 +0200

    Update post.js

--- admin/preview-templates/post.js
+++ admin/preview-templates/post.js
@@ -13,17 +13,17 @@ const Post = createClass({
         <article>
           <h1>${entry.getIn(["data", "title"], null)}</h1>
           <p>
-          <small>
-            <time
-              >${
-                format(
-                  entry.getIn(["data", "date"], new Date()),
-                  "dd MMM, yyyy"
-                )
-              }</time
-            >
-            ${" by Author"}
-          </small>
+            <small>
+              <time
+                >${
+                  format(
+                    entry.getIn(["data", "date"], new Date()),
+                    "dd MMM, yyyy"
+                  )
+                }</time
+              >
+              ${" by Author"}
+            </small>
           </p>
 
           <p>${entry.getIn(["data", "summary"], "")}</p>
